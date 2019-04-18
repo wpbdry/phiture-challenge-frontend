6eea89ff7396b1ea5d0f814d59192a558b303af9
William Dry <wpbdry@gmail.com>
2019-04-18 21:30:48 +0200

    add dummy js file

---
+++ main.js
@@ -0,0 +1,14 @@
+/*************************************************************************
+_/                _/  _/_/_/    _/_/_/    _/_/_/    _/_/_/    _/      _/  
+_/              _/   _/    _/  _/    _/  _/    _/  _/    _/    _/  _/     
+_/    _/_/    _/    _/_/_/    _/_/_/    _/    _/  _/_/_/        _/        
+_/  _/  _/  _/     _/        _/    _/  _/    _/  _/  _/        _/         
+_/_/    _/_/      _/        _/_/_/    _/_/_/    _/    _/      _/  made it!
+*************************************************************************/
+
+document.getElementById("search-form").addEventListener("submit", function(event){
+    event.preventDefault();
+    const searchTerm = document.getElementById("search-input").value;
+    var resultsDiv = document.getElementById("results-div");
+    resultsDiv.innerText = "You searched for: " + searchTerm;
+  });
