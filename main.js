6c1f6fefbaa88a623d1a024082119813603d505a
William Dry <wpbdry@gmail.com>
2019-04-23 14:13:23 +0200

    create end to end transfer of data

--- main.js
+++ main.js
@@ -6,17 +6,34 @@ _/  _/  _/  _/     _/        _/    _/  _/    _/  _/  _/        _/
 _/_/    _/_/      _/        _/_/_/    _/_/_/    _/    _/      _/  made it!
 *************************************************************************/
 
+function displaySearchResults(apiReturn) {
+  var resultsDiv = document.getElementById("results-div");
+  //const textResult = JSON.stringify(apiReturn);
+  const textResult = apiReturn.result;
+  resultsDiv.innerText = "The server returned: " + textResult;
+};
+
+async function sendSearchRequest(searchTerm) {
+  const payload = {
+    searchterm: searchTerm
+  }
+  const response = await fetch(searchApiUrl, {
+    method: "POST",
+    cache: "no-cache",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    redirect: "follow",
+    referrer: "no-referrer",
+    body: JSON.stringify(payload)
+  });
+  return await response.json();
+};
+
 document.getElementById("search-form").addEventListener("submit", function(event){
     event.preventDefault();
     const searchTerm = document.getElementById("search-input").value;
-    var resultsDiv = document.getElementById("results-div");
-    resultsDiv.innerText = "You searched for: " + searchTerm;
-
-    fetch(searchApiUrl)
-    .then(function(response) {
-      return response.json();
-    })
-    .then(function(myJson) {
-      console.log(myJson);
-    });
+    sendSearchRequest(searchTerm)
+    .then(data => displaySearchResults(data)) //try changing data to payload
+    .catch(error => console.error(error));
   });
